49c3b0670d79962850f471103fd852bc7ccec124
Sonia Xu <soniaxu7@foxmail.com>
2019-03-13 17:35:27 -0400

    add layouts

--- static/scripts/main.jsx
+++ static/scripts/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Header from './components/header';
 import { Button } from 'react-bootstrap';
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Shopping List for hi</h1>
-        <Button size="sm">test</Button>
+        <Header />
+        <Button size="sm">Add Dataset</Button>
       </div>
     );
   }
